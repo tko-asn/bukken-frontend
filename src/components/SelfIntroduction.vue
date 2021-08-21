66f4b89890845d512636fb44168892af0499352f
takasany <jackmacman2@gmail.com>
2021-08-21 17:06:01 +0900

    Create for display on my page #13

---
+++ src/components/SelfIntroduction.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- 自己紹介 -->
+  <div>
+    <div class="self_introduction" v-if="selfIntroduction">
+      <p>{{ selfIntroduction }}</p>
+    </div>
+    <p class="no_self_introduction" v-else>自己紹介はありません</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    selfIntroduction: String,
+  },
+};
+</script>
+
+<style scoped>
+/* 自己紹介 */
+.self_introduction {
+  padding: 10px 50px;
+  overflow-wrap: break-word;
+  background: #fff;
+  font-size: 1.1em;
+}
+
+.no_self_introduction {
+  text-align: center;
+}
+</style>
