42f1956687daf9f916abf4a17adeea81ca26fa1e
takasany <jackmacman2@gmail.com>
2021-09-14 22:06:17 +0900

    最新の投稿を取得するための機能を追加

--- src/store/modules/posts.js
+++ src/store/modules/posts.js
@@ -2,54 +2,78 @@ import apiClient from '@/axios';
 import store from '@/store';
 
 const state = {
+  latestPosts: [], // ホームページで表示される最新の投稿
   myPosts: [], // 自分の投稿
   myFavoritePosts: [], // お気に入りの投稿
   followeePosts: [], // フォロイーの投稿
+  pageTotal: { // それぞれの投稿の総ページ数
+    home: 0,
+    myPosts: 0,
+    followee: 0,
+    favorites: 0,
+  }
 };
 
 const getters = {
+  latestPosts: state => state.latestPosts,
   myPosts: state => state.myPosts,
   myFavoritePosts: state => state.myFavoritePosts,
   followeePosts: state => state.followeePosts,
+  pageTotal: state => state.pageTotal
 };
 
 const mutations = {
+  setLatestPosts(state, payload) { // 最新の投稿をセット
+    state.latestPosts = payload.posts;
+    state.pageTotal.home = payload.total;
+  },
   setMyPosts(state, payload) { // 自分の投稿をセット
-    state.myPosts = payload;
+    state.myPosts = payload.posts;
+    state.pageTotal.myPosts = payload.total; // 総ページ数
   },
   setMyFavoritePosts(state, payload) { // お気に入りの投稿をセット
-    state.myFavoritePosts = payload;
+    state.myFavoritePosts = payload.posts;
+    state.pageTotal.favorites = payload.total;
   },
   setFolloweePosts(state, payload) { // フォローしているユーザーの投稿をセット
-    state.followeePosts = payload;
+    state.followeePosts = payload.posts;
+    state.pageTotal.followee = payload.total;
   },
 };
 
 const actions = {
+  // 最新の投稿を取得
+  getLatestPosts({ commit }, page = 1) {
+    return apiClient.get('/posts/page/' + page + '/')
+      .then(response => {
+        commit('setLatestPosts', response.data);
+      })
+      .catch(err => Promise.reject(err));
+  },
   // 自分の投稿を取得
-  getMyPosts({ commit }) {
-    return apiClient.get('/posts/' + store.getters['auth/userId'] + '/')
+  getMyPosts({ commit }, page = 1) {
+    return apiClient.get('/posts/' + store.getters['auth/userId'] + '/page/' + page + '/')
       .then(response => {
         commit('setMyPosts', response.data);
       })
       .catch(err => Promise.reject(err));
   },
   // ユーザーのお気に入りの投稿を取得
-  getMyFavoritePosts({ commit }) {
-    return apiClient.get('/posts/favorite/user/' + store.getters['auth/userId'] + '/page/1')
+  getMyFavoritePosts({ commit }, page = 1) {
+    return apiClient.get('/posts/favorite/user/' + store.getters['auth/userId'] + '/page/' + page + '/')
       .then(response => {
         commit('setMyFavoritePosts', response.data);
       })
       .catch(err => Promise.reject(err));
   },
   // フォローしているユーザーの投稿を取得
-  async getFolloweePosts({ commit }) {
+  async getFolloweePosts({ commit }, page = 1) {
     const followsId = [];
     for (const obj of store.getters['follows/follow']) {
       followsId.push(obj.follow.id); // followカラムのユーザーのIDをセット
     }
     // フォロワーを取得
-    const res = await apiClient.post('/posts/followee/', { followsId })
+    const res = await apiClient.post('/posts/followee/page/' + page + '/', { followsId })
       .catch(err => Promise.reject(err))
 
     // Vuexにセット
