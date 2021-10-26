39fbdb2b521b530572ef071a2f97808a7cfe6a49
takasany <jackmacman2@gmail.com>
2021-10-27 03:35:15 +0900

    質問編集機能を追加 #90

--- src/pages/PostDetails.vue
+++ src/pages/PostDetails.vue
@@ -2,84 +2,153 @@
   <div class="container">
     <div class="container__item-post-details">
       <article class="block-content">
-        <!-- タイトル -->
-        <p class="block-content__title">{{ post.title }}</p>
-        <!-- 物件情報 -->
-        <div class="item-property">
-          <h4 class="item-property__title">対象物件</h4>
-          <!-- 物件名 -->
-          <p class="item-property__text">{{ post.property }}</p>
-          <!-- 物件住所 -->
-          <p class="item-property__text">
-            {{ postalCodeData(post.address) }}
-            <span>{{ addressData(post.address) }}</span>
+        <!-- 通常時 -->
+        <template v-if="!isEditingPost">
+          <!-- タイトル -->
+          <p class="block-content__title">
+            {{ post.title }}
           </p>
-          <a
-            class="item-property__link-map"
-            :href="
-              'https://maps.google.co.jp/maps/search/' +
-              this.addressData(post.address)
-            "
-            target="_blank"
-            rel="noopener noreferrer"
-            >地図を表示</a
-          >
-        </div>
-        <p class="block-content__updated-at">{{ datetime(post.updatedAt) }}</p>
-        <!-- 内容 -->
-        <p class="block-content__text">{{ post.text }}</p>
-        <!-- 下部 -->
-        <div class="item-bottom">
-          <!-- お気に入りボタン -->
-          <a
-            class="item-bottom__btn"
-            href=""
-            @click.prevent="createFavoritePost"
-            v-show="!isYourFavoritePost"
-            >お気に入りに追加</a
-          >
-          <a
-            class="item-bottom__btn"
-            href=""
-            @click.prevent="deleteFavoritePost"
-            v-show="isYourFavoritePost"
-            >お気に入り解除</a
-          >
-          <!-- 投稿者 -->
-          <div
-            class="item-bottom__author"
-            @click="moveToUserPage(post.user.id)"
-          >
-            <!-- アイコン -->
-            <div class="block-icon">
-              <img
-                class="block-icon__img"
-                :src="post.user.icon_url"
-                v-if="post.user.icon_url"
+          <!-- 物件情報 -->
+          <div class="item-property">
+            <h4 class="item-property__title">対象物件</h4>
+            <div class="item-proprety__body--no_editing">
+              <!-- 物件名 -->
+              <p class="item-property__text">{{ post.property }}</p>
+              <!-- 物件住所 -->
+              <p class="item-property__text">
+                {{ "〒" + postalCodeA(post) + "-" + postalCodeB(post) }}
+                <span>{{ addressData(post) }}</span>
+              </p>
+              <a
+                class="item-property__link-map"
+                :href="
+                  'https://maps.google.co.jp/maps/search/' + addressData(post)
+                "
+                target="_blank"
+                rel="noopener noreferrer"
+                >地図を表示</a
+              >
+            </div>
+          </div>
+          <p class="block-content__updated-at">
+            {{ datetime(post.createdAt) }}
+            <span
+              class="block-content__span"
+              v-if="post.createdAt != post.updatedAt"
+              >(編集済み)</span
+            >
+          </p>
+          <!-- 内容（改行するとスペースが入る） -->
+          <p class="block-content__text">{{ post.text }}</p>
+          <!-- 下部 -->
+          <div class="item-bottom">
+            <!-- お気に入りボタン -->
+            <a
+              class="item-bottom__btn"
+              href=""
+              @click.prevent="createFavoritePost"
+              v-show="!isYourFavoritePost"
+              >お気に入りに追加</a
+            >
+            <a
+              class="item-bottom__btn"
+              href=""
+              @click.prevent="deleteFavoritePost"
+              v-show="isYourFavoritePost"
+              >お気に入り解除</a
+            >
+            <!-- 投稿者 -->
+            <div
+              class="item-bottom__author"
+              @click="moveToUserPage(post.user.id)"
+            >
+              <!-- アイコン -->
+              <div class="block-icon">
+                <img
+                  class="block-icon__img"
+                  :src="post.user.icon_url"
+                  v-if="post.user.icon_url"
+                />
+              </div>
+              <!-- ユーザー名 -->
+              <p class="item-bottom__username">{{ post.user.username }}</p>
+            </div>
+          </div>
+          <!-- カテゴリー -->
+          <div class="item-category">
+            <div class="item-category__body--no_editing">
+              <p class="item-category__title" v-show="categoryLength">
+                カテゴリ―
+              </p>
+              <span
+                class="item-category__item"
+                v-for="category in post.categories"
+                :key="category.secondCategory"
+              >
+                {{ category.firstCategory
+                }}<span
+                  class="item-category__span"
+                  v-if="category.secondCategory"
+                  >/</span
+                >{{ category.secondCategory }}
+              </span>
+            </div>
+          </div>
+        </template>
+
+        <!-- 編集時 -->
+        <template v-else>
+          <!-- タイトル -->
+          <input
+            class="block-content__input-title block-content__title"
+            type="text"
+            v-model="editPostData.title"
+            placeholder="タイトル"
+          />
+          <!-- 物件情報 -->
+          <div class="item-property">
+            <h4 class="item-property__title">物件情報</h4>
+            <div class="item-property__body--editing">
+              <input
+                type="text"
+                class="block-content__input-property"
+                v-model="editPostData.property"
+                placeholder="物件名"
+              />
+              <AddressForm
+                :addressProps="addressDataProps"
+                :optionInput="true"
+                :validations="addressMessages"
+                @addressData="getAddressData"
               />
             </div>
-            <!-- ユーザー名 -->
-            <p class="item-bottom__username">{{ post.user.username }}</p>
           </div>
-        </div>
-        <!-- カテゴリ― -->
-        <div class="item-category">
-          <p class="item-category__title" v-show="categoryLength(post)">
-            カテゴリ―
-          </p>
-          <span
-            class="item-category__item"
-            v-for="category in post.categories"
-            :key="category.secondCategory"
-          >
-            {{ category.firstCategory }}/{{ category.secondCategory }}
-          </span>
-        </div>
+          <!-- 内容 -->
+          <textarea
+            class="block-content__input-text"
+            cols="30"
+            rows="10"
+            v-model="editPostData.text"
+            placeholder="質問内容"
+          ></textarea>
+          <!-- カテゴリー -->
+          <div class="item-category">
+            <!-- 編集モード -->
+            <div class="item-category__body--editing">
+              <CategoryForm
+                :selectedCategories="editCategoryData"
+                @add="addCategory"
+              />
+              <Tag :tags="editCategoryData" :deleteTag="removeCategory" />
+            </div>
+          </div>
+        </template>
 
         <!-- 回答フォーム -->
         <div class="form-answer">
-          <!-- ログイン済み -->
-          <template v-if="isLoggedIn">
+          <!-- ログイン済みかつ自分の質問でない場合 -->
+          <template v-if="isLoggedIn && !isYourPost">
+            <!-- まだ回答していない場合 -->
             <template v-if="!haveYouAnswered">
               <textarea
                 class="form-answer__textarea"
@@ -95,9 +164,37 @@
               />
               <button class="form-answer__btn" @click="postAnswer">回答</button>
             </template>
+            <!-- 既に回答している場合 -->
             <p class="form-answer__text form-answer__text--answered" v-else>
               この質問には回答済みです
             </p>
+          </template>
+
+          <!-- ログイン済みかつ自分の質問の場合 -->
+          <template v-else-if="isLoggedIn && isYourPost">
+            <!-- 編集モードでない場合 -->
+            <button
+              class="form-answer__btn"
+              @click="isEditingPost = true"
+              v-show="!isEditingPost"
+            >
+              投稿を編集する
+            </button>
+            <!-- 編集モード -->
+            <div class="form-answer__edit-post" v-show="isEditingPost">
+              <button
+                class="form-answer__btn form-answer__btn--half_cancel"
+                @click="isEditingPost = false"
+              >
+                キャンセル
+              </button>
+              <button
+                class="form-answer__btn form-answer__btn--half_edit"
+                @click="updatePost"
+              >
+                編集
+              </button>
+            </div>
           </template>
 
           <!-- 未ログイン -->
@@ -168,7 +265,12 @@ import { mapGetters, mapActions } from "vuex";
 import apiClient from "@/axios";
 import ValidationMessage from "@/components/ValidationMessage";
 import SidePostList from "@/components/SidePostList";
+import AddressForm from "@/components/AddressForm";
+import CategoryForm from "@/components/CategoryForm";
+import Tag from "@/components/Tag";
 import moment from "moment";
+import addressValidationMixin from "@/mixins/addressValidationMixin";
+import addressData from "@/mixins/addressData";
 
 export default {
   props: {
@@ -177,7 +279,11 @@ export default {
   components: {
     ValidationMessage,
     SidePostList,
+    AddressForm,
+    CategoryForm,
+    Tag,
   },
+  mixins: [addressValidationMixin, addressData],
   data() {
     return {
       // 参照エラーが発生するので空のデータを追加
@@ -187,6 +293,7 @@ export default {
         title: "",
         text: "",
         updatedAt: "",
+        createdAt: "",
         user: {
           id: "",
           icon_url: "",
@@ -209,11 +316,34 @@ export default {
       },
       newAnswer: "", // 新しい回答文
       answerValidations: [], // 回答のバリデーションメッセージ
+      // 更新用質問データ
+      editPostData: {
+        title: "",
+        property: "",
+        text: "",
+      },
+      editAddressData: {},
+      editCategoryData: [],
+      isEditingPost: false, // 質問を編集中かどうか
+      editFlag: {
+        address: false,
+        category: false,
+      },
+      editCategory: false, // カテゴリーを編集したかどうか
     };
   },
   created() {
     apiClient.get("/posts/post/" + this.postId + "/").then(({ data }) => {
       this.post = data; // 対象の投稿データをセット
+
+      // 編集用データ
+      this.editPostData.title = data.title;
+      this.editPostData.text = data.text;
+      this.editPostData.property = data.property;
+      this.editAddressData = { ...data.address };
+      this.editCategoryData = [...data.categories];
+      this.editAddressData.postalCodeA = this.postalCodeA(this.post);
+      this.editAddressData.postalCodeB = this.postalCodeB(this.post);
     });
   },
   computed: {
@@ -221,43 +351,41 @@ export default {
     ...mapGetters("posts", ["myFavoritePosts"]),
     // ユーザーのお気に入りの投稿かどうか
     isYourFavoritePost() {
-      if (this.myFavoritePosts.find((el) => el.id === this.postId)) {
-        return true;
-      }
-      return false;
+      return this.myFavoritePosts.some((el) => el.id === this.postId);
     },
     // 質問に回答済みかどうか
     haveYouAnswered() {
       return this.post.answers.some((el) => el.user.id === this.userId);
     },
+    // 自分の質問かどうか
+    isYourPost() {
+      return this.post.user.id === this.userId;
+    },
+    // 所在地の初期値を設定
+    addressDataProps() {
+      return {
+        postalCodeA: this.postalCodeA(this.post),
+        postalCodeB: this.postalCodeB(this.post),
+        townName: this.post.address?.["townName"],
+        buildingName: this.post.address?.["buildingName"],
+      };
+    },
+    // カテゴリーの数を返す
+    categoryLength() {
+      return this.post.categories?.length;
+    },
   },
   methods: {
     ...mapActions("posts", ["addFavoritePost", "removeFavoritePost"]),
-    // カテゴリーの数を返す
-    categoryLength(post) {
-      return post.categories?.length;
-    },
     // 時間フォーマッター
     datetime(date) {
       return moment(date).format("YYYY年MM月D日 HH時mm分");
     },
-    // 郵便番号のみ返す
-    postalCodeData(address) {
-      return (
-        "〒" +
-        address?.["postalCode"].slice(0, 3) +
-        "-" +
-        address?.["postalCode"].slice(3)
-      );
-    },
-    // 所在地のデータを連結して返す
-    addressData(address) {
-      return (
-        address?.["prefecture"] +
-        address?.["municipality"] +
-        address?.["townName"] +
-        address?.["buildingName"]
-      );
+    // AddressFormから所在地データを取得
+    getAddressData(addressObj) {
+      Object.keys(addressObj).forEach((key) => {
+        this.editAddressData[key] = addressObj[key];
+      });
     },
     // お気に入りの投稿を追加
     createFavoritePost() {
@@ -284,6 +412,71 @@ export default {
         name: "userView",
         params: { id },
       });
+    },
+    // 選択中のカテゴリを削除
+    removeCategory(id) {
+      this.editCategory = true;
+      this.editCategoryData = this.editCategoryData.filter(
+        (el) => el.id !== id
+      );
+    },
+    // カテゴリーの追加
+    addCategory(newCategory) {
+      this.editCategory = true;
+      this.editCategoryData.push(newCategory);
+    },
+    // 投稿を編集する
+    async updatePost() {
+      // 住所に変更がある場合
+      if (
+        this.editAddressData.postalCode !== this.post.address.postalCode ||
+        this.editAddressData.townName !== this.post.address.townName ||
+        this.editAddressData.buildingName !== this.post.address.buildingName
+      ) {
+        // バリデーションを実行
+        this.addressValidation(this.editAddressData);
+        const { data } = await apiClient.post(
+          // 住所のIDを取得
+          "/addresses/find/or/create/",
+          this.editAddressData
+        );
+        this.editPostData.addressId = data.addressId;
+      }
+
+      // 投稿を編集
+      await apiClient.patch(
+        "/posts/update/" + this.post.id + "/",
+        this.editPostData
+      );
+
+      if (this.editCategory) {
+        // 投稿のカテゴリーを全削除
+        await apiClient.delete("/posts/" + this.post.id + "/remove/categories");
+        // カテゴリーのIDのリストを取得
+        const { data } = await apiClient.post(
+          "/categories/find/or/create/",
+          this.editCategoryData
+        );
+        // 投稿モデルとカテゴリーモデルの中間テーブルのデータ作成
+        await Promise.all(
+          data.map(
+            async (categoryId) =>
+              await apiClient.post("/posts/set/category", {
+                postId: this.post.id,
+                categoryId,
+              })
+          )
+        );
+      }
+
+      // 投稿内容を更新
+      const { data } = await apiClient.get("/posts/post/" + this.postId + "/");
+      this.post = data;
+
+      // フラグを初期化
+      this.editCategory = false;
+
+      this.isEditingPost = false;
     },
     // 回答を投稿する
     async postAnswer() {
@@ -426,8 +619,38 @@ a {
 /* 内容 */
 .block-content__text {
   min-height: 50px;
-  margin: 30px 0;
+  margin: 20px 0;
   white-space: pre-wrap;
+  font-size: 0.9em;
+}
+
+/* 編集フォーム */
+[class*="block-content__input"] {
+  width: 100%;
+  border: 2px solid rgb(143, 142, 142);
+  box-sizing: border-box;
+  border-radius: 5px;
+}
+
+.block-content__input-title {
+  height: 40px;
+}
+
+.block-content__input-property {
+  height: 30px;
+  margin: 10px 0;
+}
+
+.block-content__input-text {
+  margin: 20px 0;
+}
+
+.form-answer__edit-post {
+  display: flex;
+}
+
+[class*="form-answer__btn--half"] {
+  width: 50%;
 }
 
 /* 物件情報 */
@@ -452,6 +675,10 @@ a {
   font-size: 0.8em;
 }
 
+.block-content__span {
+  display: inline-block;
+}
+
 /* コンテンツ下部 */
 .item-bottom {
   display: flex;
@@ -462,7 +689,7 @@ a {
 
 /* コンテンツ内ボタン */
 .item-bottom__btn {
-  padding: 10px 20px;
+  padding: 5px 10px;
   border: 2.4px solid rgba(83, 83, 224, 0.808);
   border-radius: 5px;
   color: rgba(83, 83, 224, 0.808);
@@ -482,7 +709,7 @@ a {
 .item-bottom__author {
   display: flex;
   align-items: center;
-  padding: 0 20px;
+  padding: 0 10px;
   background: rgba(165, 167, 165, 0.315);
   cursor: pointer;
 }
@@ -511,7 +738,7 @@ a {
 }
 
 /* カテゴリ― */
-.item-category {
+.item-category__body--no_editing {
   font-size: 0.8em;
 }
 
@@ -563,12 +790,20 @@ a {
   border-radius: 5px;
   background: rgb(172, 21, 192);
   color: white;
-  letter-spacing: 10px;
+  letter-spacing: 2px;
   cursor: pointer;
 }
 
 .form-answer__btn:hover {
   opacity: 0.8;
+}
+
+.form-answer__btn--half_cancel {
+  background: gray;
+}
+
+.form-answer__btn--half_edit {
+  background: rgb(27, 235, 27);
 }
 
 .form-answer__validation {
