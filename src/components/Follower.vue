bdc136e7dacd1bd1c35400cea861951b593e1aeb
takasany <jackmacman2@gmail.com>
2021-11-10 17:10:26 +0900

    コンテンツタイトル用コンポーネントとコンテンツ用コンポーネントを使ってコードを修正 #116

--- src/components/Follower.vue
+++ src/components/Follower.vue
@@ -1,28 +1,41 @@
 <template>
   <div class="block-follower">
-    <FollowsList :followsList="followers" />
-    <Pagination
-      class="block-follower__pagination"
-      :total="followTotal.follower"
-      :pageNumber="followPage.follower"
-      :paginationFunc="pagination"
-    />
+    <ContentTitle title="フォロワー" :showMenuFunc="showMenuFunc" />
+    <Content :isScroll="true">
+      <div class="container-follower">
+        <FollowsList :followsList="followers" />
+        <Pagination
+          class="container-follower__pagination"
+          :total="followTotal.follower"
+          :pageNumber="followPage.follower"
+          :paginationFunc="pagination"
+        />
+      </div>
+    </Content>
   </div>
 </template>
 
 <script>
 import FollowsList from "@/components/FollowsList";
 import Pagination from "@/components/Pagination";
+import ContentTitle from "@/components/ContentTitle";
+import Content from "@/components/Content";
 import followData from "@/mixins/followData";
 
 export default {
   components: {
     FollowsList,
     Pagination,
+    ContentTitle,
+    Content,
   },
   mixins: [followData],
   props: {
     userIdProps: String,
+    showMenuFunc: {
+      type: Function,
+      default: () => {},
+    },
   },
   created() {
     this.getFollowers(1, this.userIdProps);
@@ -36,7 +49,7 @@ export default {
 </script>
 
 <style scoped>
-.block-follower__pagination {
+.container-follower__pagination {
   padding: 5px 0;
 }
 </style>
