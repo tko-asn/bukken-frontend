b753f4ed49fe696e10255f863fdb69ad9838e931
takasany <jackmacman2@gmail.com>
2021-11-10 17:12:12 +0900

    回答表示コンテンツのコンポーネントを作成 #116

---
+++ src/components/AnswerListContent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="block-answer-list-content">
+    <ContentTitle :title="title" :showMenuFunc="showMenuFunc" />
+    <Content :isScroll="true">
+      <AnswerList :answerType="answerType" :userIdProps="userIdProps" />
+    </Content>
+  </div>
+</template>
+
+<script>
+import ContentTitle from "@/components/ContentTitle";
+import Content from "@/components/Content";
+import AnswerList from "@/components/AnswerList";
+
+export default {
+  components: {
+    ContentTitle,
+    Content,
+    AnswerList,
+  },
+  props: {
+    answerType: String,
+    userIdProps: String,
+    showMenuFunc: {
+      type: Function,
+      default: () => {},
+    },
+  },
+  computed: {
+    title() {
+      return this.answerType === "answer" ? "ユーザーの回答" : "いいねした回答";
+    },
+  },
+};
+</script>
