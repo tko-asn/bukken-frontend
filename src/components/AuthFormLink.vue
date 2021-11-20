c14110e95a05acc2494f303bee150fba288f4ccb
takasany <jackmacman2@gmail.com>
2021-11-20 20:00:27 +0900

    認証ページのリンク用コンポーネントを作成 #127

---
+++ src/components/AuthFormLink.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="block-auth-form-link">
+    <p class="block-auth-form-link__text">{{ linkObj.label }}</p>
+    <router-link
+      class="block-auth-form-link__link"
+      :to="{ name: linkObj.name }"
+      >{{ linkObj.value }}</router-link
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    linkObj: {
+      type: Object,
+      default: () => ({ label: "", name: "", value: "" }),
+    },
+  },
+};
+</script>
+
+
+<style scoped>
+.block-auth-form-link {
+  height: 100px;
+  color: gray;
+  font-size: 1.2em;
+  letter-spacing: 2px;
+  text-align: center;
+}
+
+.block-auth-form-link__text {
+  margin: 30px 0 10px;
+  font-size: 0.8em;
+}
+
+.block-auth-form-link__link {
+  color: rgb(52, 189, 161);
+  text-decoration: none;
+  font-size: 0.9em;
+}
+</style>
